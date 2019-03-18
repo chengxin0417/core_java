9d53a1aceae939c338b055bfa6f00c3f3cc4b537
yinchu <javayinchu2013@163.com>
2019-03-18 22:27:36 +0800

    updated

---
+++ src/core_java/operation/NotEqualOperation.java
@@ -0,0 +1,18 @@
+package core_java.operation;
+
+/**
+ *  ！操作符代码示例
+ * @author yinchu
+ *
+ */
+public class NotEqualOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean flag = false;
+		if(!flag) {
+			System.out.println("进入了if语句块...");
+		}
+	}
+
+}
