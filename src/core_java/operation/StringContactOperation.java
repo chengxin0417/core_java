1080a44d0c60f7368d74a6ac285361d563d96160
yinchu <javayinchu2013@163.com>
2019-03-18 22:46:16 +0800

    updated

---
+++ src/core_java/operation/StringContactOperation.java
@@ -0,0 +1,22 @@
+package core_java.operation;
+
+/**
+ * 字符串拼接操作符+代码示例
+ * 
+ * @author yinchu
+ *
+ */
+public class StringContactOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String prefixWord = "My name is ";
+		String name = "古天乐";
+		System.out.println(prefixWord + name);
+
+		String s1 = "abc_";
+		int i = 1;
+		System.out.println(s1 + i);
+	}
+
+}
