cf796e626aa03bd900f8ceb826efb7450e78f351
yinchu <javayinchu2013@163.com>
2019-03-18 22:56:58 +0800

    updated

---
+++ src/core_java/operation/DoubleEqualsOperation.java
@@ -0,0 +1,20 @@
+package core_java.operation;
+
+/**
+ * 操作符==代码示例
+ * @author yinchu
+ *
+ */
+public class DoubleEqualsOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = new String("abc");
+		String s2 = new String("abc");
+		String s3 = s1;
+		System.out.println(s1==s2);
+		System.out.println(s1==s3);
+		
+	}
+
+}
