94130fd2522e02d2da19449b43ea53547f56b57f
yinchu <javayinchu2013@163.com>
2019-03-18 15:02:23 +0800

    updated

---
+++ src/core_java/operation/DoubleAddOperation.java
@@ -0,0 +1,20 @@
+package core_java.operation;
+
+public class DoubleAddOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1;
+		int b = 1;
+		int c = 1;
+		int d = 1;
+		
+		c = a++;
+		d = ++b;
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+		System.out.println("d = " + d);
+	}
+
+}
