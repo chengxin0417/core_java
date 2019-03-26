2d5d750b505d4aed828db4b9375eb51789918962
yinchu <javayinchu2013@163.com>
2019-03-26 14:01:29 +0800

    updated

---
+++ src/core_java/flow_control/for_loop/ForLoopDemo.java
@@ -0,0 +1,13 @@
+package core_java.flow_control.for_loop;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = new int[]{1,2,5,6,8,9,9,0,2,3,0,4,5,6,77,99};
+		for(int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+
+}
