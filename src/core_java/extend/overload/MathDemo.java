3e52e8f91582f5922c2560d427c0f0d94b9689ea
yinchu <javayinchu2013@163.com>
2019-03-28 10:06:56 +0800

    updated

---
+++ src/core_java/extend/overload/MathDemo.java
@@ -0,0 +1,13 @@
+package core_java.extend.overload;
+
+public class MathDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.max(1.0f, 1.1f));
+		System.out.println(Math.max(1, 2));
+		
+		//Math.max(1.1,9);
+	}
+
+}
