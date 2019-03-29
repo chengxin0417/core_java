e49b715c33966c2f6ed69badee1b439f641703da
yinchu <javayinchu2013@163.com>
2019-03-29 11:40:03 +0800

    updated

---
+++ src/core_java/extend/overwrite/BaseClass.java
@@ -0,0 +1,8 @@
+package core_java.extend.overwrite;
+
+public class BaseClass {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+}
---
+++ src/core_java/extend/overwrite/ChildClass.java
@@ -0,0 +1,8 @@
+package core_java.extend.overwrite;
+
+public class ChildClass extends BaseClass {
+	
+	public int add(int a, int b) {
+		return a + b + 100;
+	}
+}
