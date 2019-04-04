73d11ae02facd38f28456c4e7b4bb4dec5dd748d
yinchu <javayinchu2013@163.com>
2019-04-04 22:55:06 +0800

    updated

---
+++ src/core_java/modifier/abstract_class/AbstractClass.java
@@ -0,0 +1,11 @@
+package core_java.modifier.abstract_class;
+
+public abstract class AbstractClass {
+	
+	public void print(String name) {
+		System.out.println("这是个抽象类...");
+	}
+	
+	public abstract void acs_method() ;
+	
+}
