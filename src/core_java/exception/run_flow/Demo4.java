88234f795d76a1d4df5cfa8513c671381b50a75b
yinchu <javayinchu2013@163.com>
2019-04-08 14:07:05 +0800

    updated

--- src/core_java/exception/run_flow/Demo4.java
+++ src/core_java/exception/run_flow/Demo4.java
@@ -4,7 +4,7 @@ public class Demo4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		method();
+		System.out.println(method());
 		System.out.println("End");
 	}
 
