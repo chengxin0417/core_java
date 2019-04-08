3d11a3bf8b1186aea677d940d1435d7871a61b5f
yinchu <javayinchu2013@163.com>
2019-04-08 13:51:58 +0800

    updated

---
+++ src/core_java/exception/run_flow/Demo4.java
@@ -0,0 +1,25 @@
+package core_java.exception.run_flow;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		method();
+		System.out.println("End");
+	}
+
+	public static String method() {
+		String x = null;
+		try {
+			System.out.println("a");
+			System.out.println(x.length());
+			return "try";
+		} catch (Exception e) {
+			System.out.println("b");
+			return "catch";
+		} finally {
+			System.out.println("c");
+		}
+	}
+
+}
