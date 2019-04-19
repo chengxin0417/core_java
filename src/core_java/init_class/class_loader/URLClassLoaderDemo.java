09ed2c615c98b61e1f2fe02d9a4938696ad9eb3b
yinchu <javayinchu2013@163.com>
2019-04-19 19:41:53 +0800

    updated

---
+++ src/core_java/init_class/class_loader/URLClassLoaderDemo.java
@@ -0,0 +1,17 @@
+package core_java.init_class.class_loader;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class URLClassLoaderDemo {
+
+	public static void main(String[] args) throws MalformedURLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+		// TODO Auto-generated method stub
+		URL url = new URL("https://github.com/chengxin0417/core_java");
+		URLClassLoader loader = new URLClassLoader(new URL[] {url});
+		Class objClass = loader.loadClass("core_java.init_class.final_var.Tester");
+		Object obj = objClass.newInstance();
+	}
+
+}
